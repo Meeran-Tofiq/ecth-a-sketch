48194162a0b93c78273510a87de098992f6fe6a5
Meeran <101665499+Meeran420@users.noreply.github.com>
2022-12-15 21:46:05 +0300

    Make createGrid function
    It's a function that makes a 16x16 grid. The divs each have a class and
    ID representative of the (x, y) coordinate of the div.

---
+++ javascipt.js
@@ -0,0 +1,29 @@
+const container = document.querySelector("#grid-container");
+const gridButton = document.querySelector('#create-grid');
+
+gridButton.addEventListener('click', () => {
+    createGrid();
+});
+
+function createGrid() {
+    for(let y = 0; y < 16; y++) {
+        let yCont = document.createElement('div');
+        yCont.classList.add('y');
+        yCont.setAttribute('id', y + 1);
+        for(let x = 0; x < 16; x++) {
+            let xCont = document.createElement('div');
+
+            xCont.classList.add('x');
+            xCont.setAttribute('id', x + 1)
+
+            xCont.textContent = (x + 1);
+
+            yCont.appendChild(xCont);
+        }
+        container.appendChild(yCont);
+    }
+
+    let p = document.createElement('p');
+    p.textContent = 'Hello there!';
+    container.appendChild(p);
+}
