c0dd5afafd5f431061c63b60035c85e752aa336e
Meeran <101665499+Meeran420@users.noreply.github.com>
2022-12-15 22:32:09 +0300

    Fix bug where new grid would appear below old one

--- javascipt.js
+++ javascipt.js
@@ -2,15 +2,23 @@ const container = document.querySelector("#grid-container");
 const gridButton = document.querySelector('#create-grid');
 
 gridButton.addEventListener('click', () => {
+    var child = container.lastElementChild; 
+        while (child) {
+            container.removeChild(child);
+            child = container.lastElementChild;
+        }
     createGrid();
 });
 
 function createGrid() {
-    for(let y = 0; y < 16; y++) {
+    let size = askUserForSize();
+
+    for(let y = 0; y < size; y++) {
         let yCont = document.createElement('div');
         yCont.classList.add('y');
         yCont.setAttribute('id', y + 1);
-        for(let x = 0; x < 16; x++) {
+
+        for(let x = 0; x < size; x++) {
             let xCont = document.createElement('div');
 
             xCont.classList.add('x');
@@ -21,6 +29,15 @@ function createGrid() {
         }
         container.appendChild(yCont);
     }
+}
+
+function askUserForSize() {
+    let s = prompt("How big do you want the grid to be?");
+    while(s > 100 || s <= 0){
+        s = prompt("Please keep the size between 1 and 100: ");
+    } 
+
+    return s;
 }
 
 function addHoverToGrid(_div) {
